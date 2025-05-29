bbbc8feed516b8ba2ada05310d5c7ebc768db653
royx0612 <royx0612@gmail.com>
2025-05-30 00:12:02 +0800

    Initial Commit

---
+++ ApexPredator/ApexPredatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApexPredatorApp.swift
+//  ApexPredator
+//
+//  Created by 呂騰耀 on 2025/5/30.
+//
+
+import SwiftUI
+
+@main
+struct ApexPredatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApexPredator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApexPredator
+//
+//  Created by 呂騰耀 on 2025/5/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
